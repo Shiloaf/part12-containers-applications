ab2b5e08b94542256abbf8c5ebeeeebdaa20b0a6
Shiloaf <ninjacornomega@gmail.com>
2025-12-17 22:36:16 +0800

    configured vitest for react testing

--- todo-app/todo-frontend/vite.config.ts
+++ todo-app/todo-frontend/vite.config.ts
@@ -1,7 +1,18 @@
-import { defineConfig } from "vite";
+/// <reference types="vitest/config" />
+import { defineConfig, mergeConfig } from "vitest/config";
+import { defineConfig as defineConfigVite } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const config = defineConfigVite({
   plugins: [react()],
 });
+
+const testConfig = defineConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./tests/setupTests.ts",
+  },
+});
+
+export default mergeConfig(config, testConfig);
